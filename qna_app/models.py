3618b4bb3929d6108a95c502ed10893a3e67e15d
Laxmi <oluxmee@gmail.com>
2019-12-11 09:47:05 +0545

    models added migrations done

--- qna_app/models.py
+++ qna_app/models.py
@@ -6,15 +6,23 @@ class QuestionModel(models.Model):
     posted_by = models.CharField(max_length=120)
     timestamp= models.DateTimeField(auto_now_add=True)
     question_desc= models.TextField()
-    ques_img=models.ImageField(upload_to="QuestionImg")
+    ques_img=models.ImageField(upload_to="QuestionImg",blank=True,null=True)
+    ques_votes=models.IntegerField(default=0)
+
+    def __str__(self):
+        return(self.title)
 
 
 class AnswerModel(models.Model):
     answer_by= models.CharField(max_length=255)
     timestamp= models.DateTimeField(auto_now_add=True)
     ans_desc= models.TextField()
-    ans_img=models.ImageField(upload_to="AnswerImg")
-    votes=models.IntegerField()
-    isaccept=models.BooleanField()
+    ans_img=models.ImageField(upload_to="AnswerImg",blank=True,null=True)
+    ans_votes=models.IntegerField(default=0)
+    isaccept=models.BooleanField(default=0)
     question= models.ForeignKey(QuestionModel,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return(self.answer_by)
     
+
